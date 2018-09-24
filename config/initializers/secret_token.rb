954829a5f112aa21aa3241a2931710ef32b2673b
Chris <chrsln93@gmail.com>
2018-09-23 21:00:09 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wellness::Application.config.secret_key_base = '361889389a99a33e5a1eb293574e5a06236d846e3b6f1f956236af0c57996723d26e62d60e2ad0b0eecbdd8e9f0db2e8d062145f80bd1621c0bbeeac36e58595'
